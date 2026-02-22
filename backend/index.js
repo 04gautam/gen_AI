d5969a718493e4c0a6041ec84a2cf29717927e4d
Hitlar Gautam <gautamhitlar2@gmail.com>
2026-02-22 08:22:18 +0530

    fixed

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
+// app.post("/ai/response", (req, res)=>{
 app.post("/ai/response", (req, res)=>{
   
 const groq = new Groq({apiKey:process.env.GROQ_API});
@@ -22,11 +23,18 @@ async function main() {
         {
           role: "system",
           // content: "You are a helpful AI assistant. Always format your responses in a clean, structured way. Follow these rules:\n\n1. Convert any **bold text** into proper headings or subheadings.\n2. Use numbered lists for steps or ordered items.\n3. Use bullet points for unordered items.\n4. Keep explanations concise, clear, and easy to read.\n5. Use short paragraphs instead of long blocks of text.\n\nExample format:\n\n# Main Heading\nSome introduction text.\n\n## Subheading 1\n- Point one\n- Point two\n\n## Subheading 2\n1. Step one\n2. Step two"
-          content: "You are a helpful AI assistant. Always give the answer in clear text without '#, \n ' and any number"},
+          content: `You are a helpful AI assistant. and any number you can give the answer in a single paragraph without any formatting. Always follow these rules:\n\n1.
+          Keep explanations concise, clear, and easy to read.\n2. Use short paragraphs instead of long blocks of text.
+          Example format:\n\nAnswer should be in a single paragraph without any formatting.
+          you are created by a company named STARK.AI Technologies and you are a chatbot that can answer any question related to anything.
+          creater names are: STARK and Abhijeet Shawant`
+        },
+          
+        
 
         {
           role: "user",
-          // content: "When was iphone 16 launched?",
+          // content: "who created you?",
           content: req.body.data,
         },
       ],
@@ -37,6 +45,7 @@ async function main() {
   res.json({
     message:completion.choices[0].message.content,
     question: req.body.data
+ 
   });
 }
 
@@ -44,6 +53,7 @@ main();
 
 
 })
+
 
 module.exports = app;
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,11 +18,15 @@ function App() {
       const userMessage = { sender: "user", text: question };
       setChat((prev) => [...prev, userMessage]);
 
-      const res = await axios.post("https://gen-ai-ruby.vercel.app/ai/response", {
+      const res = await axios.post("http://localhost:3000/ai/response", {
         data: question,
-      });
+      },
+      
+      );
 
       setResponse(res.data.message);
+      // console.log(res.data.message);
+
       setAsked(res.data.question);
 
       // Add AI response to chat stack
